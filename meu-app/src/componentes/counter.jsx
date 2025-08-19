1357754e19fbea8638170dfa18f768ef0ed4a62e
DanielDaSilveiraWerner <wernerd107@gmail.com>
2025-08-19 14:55:03 -0300

    Primeiro Comit

---
+++ meu-app/src/componentes/counter.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+export function counter(){
+
+    const [count, setCount] = useState(0)
+
+    return (
+        <>
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        </>
+    ) 
+}
